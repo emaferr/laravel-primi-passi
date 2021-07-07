3886b4990a965e55f08d48f63b9790013cc13c83
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-07-07 23:26:31 +0200

    Create product array screen mold with foreach loop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,41 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel | Home</title>
+        <title>Laravel | CBD SHOP</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" >
 
     </head>
+
     <body>
-        <h1 class="text-white bg-dark">Prova</h1>
+        <header>
+            <nav class="navbar navbar-light bg-light text-center py-4">
+                <div class="logo_bar">
+                    <a class="navbar-brand" href="#">
+                        <img src="{{ asset('img/logo.png') }}" alt="" width="50" height="50" class="d-inline-block align-text-middle">
+                    </a>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="products d-flex justify-content-between py-4">
+                @foreach ($products as $product)
+                <div class="card col-4 shadow" style="width: 18rem;">
+                    <img src="{{$product['path']}}" class="card-img-top" alt="...">
+                    <div class="card-body d-flex flex-column align-items-center">
+                        <h5 class="text-uppercase fw-bold">{{$product['name']}}</h5>
+                        <p class="card-text m-3 text-success">{{$product['details']}}</p>
+                        <span>A partire da:  <span class="fs-4 mx-2"> {{$product['price']}}€/g</span></span>
+                    </div>
+                </div>
+                @endforeach
+                </div>                
+            </div>
+        </main>
     </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $data = [
+
+        'products' => array(
+
+            [
+                'path' => 'img/Royal-gg-4-fiore-erba-legale.jpeg',
+                'name' => 'royal gg#4',
+                'details' => 'Indoor | CBD + CBDA < 25%',
+                'price' => 9.99
+            ],
+            [
+                'path' => 'img/gorilla-glue-cannabis-light-1.jpeg',
+                'name' => 'gorilla glue',
+                'details' => 'Indoor | CBD + CBDA < 22%',
+                'price' => 4.70
+            ],
+            [
+                'path' => 'img/cannabis-legale-do-si-dos.jpeg',
+                'name' => 'do si dos',
+                'details' => 'Indoor | CBD + CBDA < 22,5%',
+                'price' => 4.30
+            ]
+        )
+    ];
+
     // rename the main view to 'home'
-    return view('home');
+    return view('home',$data);
 });
